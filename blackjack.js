72b0e651a47018d8224d57c98a9ec3dbd7b0efec
Caleb <ohmk@localhost.localdomain>
2017-01-01 12:03:19 -0800

    added cards

--- blackjack.js
+++ blackjack.js
@@ -13,10 +13,39 @@ function setup() {
   //set starting status message
   var status = document.getElementById('statusMessage');
   status.innerHTML = "Welcome to Blackjack! Press New Game to get Started.";
+
+  //initialize cards names into cards array
+  var suite = ['C', 'D', 'H', 'S'];
+  var rank = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  var cards = [];
+
+  for(var i = 0; i < rank.length; i++) {
+	  for(var j = 0; j < suite.length; j++) {
+		  cards.push(rank[i] + suite[j]);
+	  }
+  }
+
+  return cards;
 }
 
-function dealCards() {
+function dealPlayerCard() {
+
+	var cardSlots = document.getElementById('dealerCards');
+
 
 }
 
-setup();
+function dealDealerCard(){
+
+}
+
+function startGame() {
+	dealPlayerCard();
+	dealDealerCard();
+	dealPlayerCard();
+	dealDealerCard();
+}
+
+
+
+console.log(setup().length);
